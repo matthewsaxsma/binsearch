0d9f5a32cd3e4375d67cf9e4ea82cfffc8d02d8c
Matt Saxsma <matthewg.saxsma@gmail.com>
2025-07-13 11:09:51 -0500

    test changes

--- R/mlr_power_bin_search.R
+++ R/mlr_power_bin_search.R
@@ -71,7 +71,7 @@ mlr_power_bin_search <- function(
 
   first_left = left
   first_right = right
-  sample_range = first_right - first_left
+  search_range = first_right - first_left
 
   if(verbose == TRUE){
     number_line <- rep("-",times = 100)
@@ -79,53 +79,43 @@ mlr_power_bin_search <- function(
     }
 
   power_args <- args_list[grep("^b|rx|alpha|datasets",names(args_list))]
-  # Exploring the search space
 
   while (left <= right) {
     middle = round((left + right) / 2)
 
-    middle_power = do.call(what=mlr_power_calc, # this is where the model power calculation function goes
-                           args=as.list(c(N = middle, power_args)))
+    middle_power = do.call(what = mlr_power_calc,
+                           args = as.list(c(N = middle, power_args)))
     if(verbose == TRUE){
-      cat(paste(rep(" ",times = length(number_line) * ((middle - first_left)/sample_range)), collapse = ""),
+      cat(paste(rep(" ",times = length(number_line) * ((middle - first_left)/search_range)), collapse = ""),
         "N = ", middle,
-        paste(rep(" ", times = length(number_line) * (1 - ((middle - first_left)/sample_range))),collapse = ""),"\r",sep = "")
+        paste(rep(" ", times = length(number_line) * (1 - ((middle - first_left)/search_range))),collapse = ""),"\r",sep = "")
       }
 
     if (middle_power < desired_power) {
-
       left = middle + 1
-
     } else if (middle_power > desired_power + 0.01) {
-
       right = middle - 1
-
     } else if (middle_power > desired_power &
                middle_power < desired_power + 0.01) {
-
       if(verbose == TRUE){
-        cat(paste(rep(" ",times = length(number_line) * ((middle - first_left)/sample_range)), collapse = ""),
+        cat(paste(rep(" ",times = length(number_line) * ((middle - first_left)/search_range)), collapse = ""),
           "N = ",middle,
-          paste(rep(" ", times = length(number_line) * (1 - ((middle - first_left)/sample_range))),collapse = ""),"\r",sep = "")
+          paste(rep(" ", times = length(number_line) * (1 - ((middle - first_left)/search_range))),collapse = ""),"\r",sep = "")
         cat("\n\nYou need ",middle," participants for ",desired_power*100,"% power to detect a standardized beta coefficient of ",b1,".\n\n",sep = "")
       }
       return(middle)
     }
   }
 
-  # Error if bounds of sample size search space is not large enough
-
   if(middle == first_left | middle == first_right){
     stop(call. = TRUE, "Adjust the bounds of your search space!")
   }
 
-  # need this condition because otherwise the algorithm might not be able to find point of 80% power.
-
   if (middle == right|middle==left){
     if(verbose == TRUE){
-      cat(paste(rep(" ",times = length(number_line) * ((middle - first_left)/sample_range)), collapse = ""),
+      cat(paste(rep(" ",times = length(number_line) * ((middle - first_left)/search_range)), collapse = ""),
         "N = ",middle,
-        paste(rep(" ", times = length(number_line) * (1 - ((middle - first_left)/sample_range))),collapse = ""),"\r",sep = "")
+        paste(rep(" ", times = length(number_line) * (1 - ((middle - first_left)/search_range))),collapse = ""),"\r",sep = "")
       cat("\n\nYou need ",middle," participants for ",desired_power*100,"% power to detect a standardized beta coefficient of ",b1,".\n\n",sep = "")
     }
     return(middle)
